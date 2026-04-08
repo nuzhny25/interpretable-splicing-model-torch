849bd93a66de0dc7c9ce1a1c63116eaea7b0d1fc
nuzhny25 <sashanuzhny@gmail.com>
2026-04-08 16:26:20 -0400

    code that I used to chunk malat1

---
+++ chunking.py
@@ -0,0 +1,20 @@
+with open('data/human_malat1.txt', 'r') as file:
+    malat1_seq = file.read()
+
+malat1_seq = malat1_seq.replace("\n", "")
+
+window_size = 70
+step_size = 10 
+
+chunks = []
+positions = []
+
+
+for i in range(0, len(malat1_seq) - window_size + 1, step_size):
+    chunk = malat1_seq[i : i + window_size]
+    chunks.append(chunk)
+    positions.append(i)
+
+import pandas as pd
+df = pd.DataFrame({"exon": chunks})
+df.to_csv("human_malat1_chunks.csv", index=False)
