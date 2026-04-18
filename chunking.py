673d6789f0caab6207c3c925cab80d8b668ad4d9
nuzhny25 <sashanuzhny@gmail.com>
2026-04-18 19:06:27 -0400

    enhanced chunking

--- chunking.py
+++ chunking.py
@@ -1,20 +1,25 @@
-with open('data/human_malat1.txt', 'r') as file:
-    malat1_seq = file.read()
+paths = ["data/human_malat1.txt", "data/mouse_malat1.txt"]
+names = ["data/human_malat1_chunks.csv", "data/mouse_malat1_chunks.csv"]
 
-malat1_seq = malat1_seq.replace("\n", "")
+for index, path in enumerate(paths):
 
-window_size = 70
-step_size = 10 
+    with open(path, "r") as file:
+        malat1_seq = file.read()
 
-chunks = []
-positions = []
+    malat1_seq = malat1_seq.replace("\n", "")
 
+    window_size = 70
+    step_size = 10
 
-for i in range(0, len(malat1_seq) - window_size + 1, step_size):
-    chunk = malat1_seq[i : i + window_size]
-    chunks.append(chunk)
-    positions.append(i)
+    chunks = []
+    positions = []
 
-import pandas as pd
-df = pd.DataFrame({"exon": chunks})
-df.to_csv("human_malat1_chunks.csv", index=False)
+    for i in range(0, len(malat1_seq) - window_size + 1, step_size):
+        chunk = malat1_seq[i : i + window_size]
+        chunks.append(chunk)
+        positions.append(i)
+
+    import pandas as pd
+
+    df = pd.DataFrame({"exon": chunks})
+    df.to_csv(names[index], index=False)
