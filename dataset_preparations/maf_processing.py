bf088ea266755c4c9c2bb17c559faf402372ba1a
nuzhny25 <sashanuzhny@gmail.com>
2026-04-22 20:14:46 -0400

    expanded s.t. it can extract multiple species sequencese

--- dataset_preparations/maf_processing.py
+++ dataset_preparations/maf_processing.py
@@ -13,28 +13,39 @@ I can find more via this link: https://genome.ucsc.edu/cgi-bin/hgc?db=hg38&c=chr
 
 """
 
+SPECIES = {
+    "human": "hg38.chr11",
+    "chimp": "panTro4.chr11",
+    "mouse": "mm10.chr19",
+    "rat": "rn6.chr1",
+    "cat": "felCat8.chrD1",
+    "elephant": "loxAfr3.scaffold_71",
+}
+
 
 def main():
     alignments = list(
         AlignIO.parse("../data/multiz100/MALAT1_orthologues_multiz100.maf", "maf")
     )
 
-    human_sequence = ""
+    sequences = {name: "" for name in SPECIES}
 
     for alignment in alignments:
         for line in alignment:
-            if line.id == "hg38.chr11":
-                human_sequence += str(line.seq)
+            for name, maf_id in SPECIES.items():
+                if line.id == maf_id:
+                    sequences[name] += str(line.seq)
 
-    human_sequence = clean_str(human_sequence)
-
-    with open("../data/multiz100/human_malat1.txt", "w") as file:
-        file.write(human_sequence)
+    for name, seq in sequences.items():
+        seq = clean_str(seq)
+        if seq:
+            with open(f"../data/multiz100/{name}_malat1.txt", "w") as file:
+                file.write(seq)
 
 
 def clean_str(string):
     return string.replace("-", "").upper().strip()
 
 
-if __name__ == main():
+if __name__ == "__main__":
     main()
